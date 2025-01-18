8ba45bb8a76ada9f46ece49ef09960eced57d131
Andrew K <ak.ibanker@gmail.com>
2025-01-18 21:59:30 +0100

    Solved daily

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -2610,4 +2610,29 @@ def doesValidArrayExist(derived: List[int]) -> bool:
     return res == 0
 
 
-print(xorAllNums([1, 2], [3, 4]))
+def minCost(grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0])
+    dist = [[10000] * n for _ in range(m)]
+    dq = collections.deque([(0, 0)])
+    dist[0][0] = 0
+    dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+
+    while dq:
+        x, y = dq.popleft()
+        curDir = grid[x][y] - 1
+
+        for dir in range(4):
+            nx, ny = x + dx[dir], y + dy[dir]
+            if 0 <= nx < m and 0 <= ny < n:
+                cost = dist[x][y] + (0 if dir == curDir else 1)
+                if cost < dist[nx][ny]:
+                    dist[nx][ny] = cost
+                    if dir == curDir:
+                        dq.appendleft((nx, ny))
+                    else:
+                        dq.append((nx, ny))
+
+    return dist[m - 1][n - 1]
+
+
+print(minCost([[1,1,3],[3,2,2],[1,1,4]]))
