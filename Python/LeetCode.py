38f9d97e435f2e86291e7c1a8f53e1135177be14
Andrew K <ak.ibanker@gmail.com>
2025-02-15 14:38:48 +0100

    Solved daily

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -3189,16 +3189,4 @@ def punishmentNumber(n: int) -> int:
     return res
 
 
-def partition(x, target):
-    if x == target: return True
-    if x == 0: return target == 0
-    for m in (10, 100, 1000):
-        if partition(x // m, target - x % m):
-            return True
-    return False
-
-
-return sum(x for i in range(1, n + 1) if partition(x := i * i, i))
-
-
 print(punishmentNumber(10))
