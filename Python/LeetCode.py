c186124dc94363bb7085de156448fd40b0a7d1a3
Andrew K <ak.ibanker@gmail.com>
2025-03-03 01:20:08 +0100

    Solved daily

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -3268,4 +3268,31 @@ def lenLongestFibSubseq(arr: List[int]) -> int:
     return res
 
 
-print(lenLongestFibSubseq([1,2,3,4,5,6,7,8]))
+def mergeArrays(nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+    res = []
+    i, j = 0, 0
+
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i][0] == nums2[j][0]:
+            res.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+            i += 1
+            j += 1
+        elif nums1[i][0] < nums2[j][0]:
+            res.append(nums1[i])
+            i += 1
+        elif nums1[i][0] > nums2[j][0]:
+            res.append(nums2[j])
+            j += 1
+
+    while i < len(nums1):
+        res.append(nums1[i])
+        i += 1
+
+    while j < len(nums2):
+        res.append(nums2[j])
+        j += 1
+
+    return res
+
+
+print(mergeArrays([[2,4],[3,6],[5,5]], [[1,3],[4,3]]))
